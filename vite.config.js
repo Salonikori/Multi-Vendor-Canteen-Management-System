688a83636cd9498ea875cbedd5e06b2d956924f1
Saloni Kori <saloni@example.com>
2026-06-17 15:56:53 +0530

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Multi-Vendor-Canteen-Management-System/',
+
   plugins: [react()],
+
   server: {
     port: 5173,
     open: true,
