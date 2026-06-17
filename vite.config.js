a455b2b90f195460fae347aba96674da16f47e2b
Saloni Kori <saloni@example.com>
2026-06-17 16:04:59 +0530

    Add GitHub Actions deployment

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/Multi-Vendor-Canteen-Management-System/',
-
   plugins: [react()],
-
   server: {
     port: 5173,
     open: true,
